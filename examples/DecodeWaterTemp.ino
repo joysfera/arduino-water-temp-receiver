e993e2d6cc93cf7fc0316e8b7051a2bd932a003e
Radiusxe <radius.soukroma@gmail.com>
2016-01-03 19:50:53 +0100

    Update DecodeWaterTemp.ino
    Added support to read negative temperatures (Hyundai WSC sensor)

--- examples/DecodeWaterTemp.ino
+++ examples/DecodeWaterTemp.ino
@@ -23,9 +23,12 @@ void printdata(byte &id, int &temp, byte &chan, boolean &batt, boolean &beep)
     Serial.print("id = ");
     Serial.print(id);
     Serial.print(", temp = ");
-    Serial.print(temp/10);
-    Serial.print(".");
-    Serial.print(temp%10);
+    Serial.print(temp/10.0,1);
     Serial.print(", chan = ");
-    Serial.println(chan);
+    Serial.print(chan);
+    Serial.print(", batt = ");  //BAT OK = 1
+    Serial.print(batt);
+    Serial.print(", beep = ");  //BEEP = 1
+    Serial.println(beep);
+
 }
