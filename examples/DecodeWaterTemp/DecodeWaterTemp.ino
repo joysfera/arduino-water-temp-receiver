5224d3fa69baa1ed4e8f1fc806b15beb2ad285e9
Petr Stehlík <petr@pstehlik.cz>
2016-05-02 20:33:43 +0200

    The example prints all the received information now. Thanks to Radiusxe for the idea of letting Serial print format the decimal number.

--- examples/DecodeWaterTemp/DecodeWaterTemp.ino
+++ examples/DecodeWaterTemp/DecodeWaterTemp.ino
@@ -1,8 +1,8 @@
 /*
  * Decode Remote Water Thermometer - KW9043
  *
- * Petr Stehlik in 2013
- * 
+ * Petr Stehlik in 2013-2016
+ *
  * GPLv3
  */
 
@@ -11,7 +11,7 @@
 void setup()
 {
     Serial.begin(115200);
-    WaterTempReceiver::init(0, printdata);
+    WaterTempReceiver::init(0, printdata);  // interrupt 0 = pin D2
 }
 
 void loop()
@@ -23,9 +23,11 @@ void printdata(byte &id, int &temp, byte &chan, boolean &batt, boolean &beep)
     Serial.print("id = ");
     Serial.print(id);
     Serial.print(", temp = ");
-    Serial.print(temp/10);
-    Serial.print(".");
-    Serial.print(temp%10);
+    Serial.print(temp / 10.0f, 1);
     Serial.print(", chan = ");
-    Serial.println(chan);
+    Serial.print(chan);
+    Serial.print(", batt = ");
+    Serial.print(batt ? "OK" : "LOW");
+    Serial.print(", beep = ");
+    Serial.println(beep ? "yes" : "no");
 }
